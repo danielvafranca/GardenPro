b0eea2bc1fbfc062219ddc2f59d1a26d90c45ade
Daniel Viotor <danielvafranca@gmail.com>
2025-01-12 21:50:41 +0000

    Adicionado Client no Model

---
+++ Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GardenPro.Models;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Adress { get; set; }
+    public string PhoneNumber{ get; set; }
+    public decimal MonthlyPayment { get; set; }
+    public int MonthlyPaymentStatus { get; set; }// 1 sim 2 nao 3 atraso
+    public int WeeklyStatusWork { get; set; }// 1 sim 2 nao 3 atraso
+
+}
