257cb6eea4bb467c45d959897e6f72308a3c2d2d
Daniel Viotor <danielvafranca@gmail.com>
2025-01-12 21:31:10 +0000

    Adicionado Roles no Models

---
+++ Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GardenPro.Models;
+public class Role
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Slug { get; set; }
+
+    public Ilist<User> Users { get; set; }
+}
+	
+
