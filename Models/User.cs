fb81c679dd541e586470cc6209edbc81c6d5be5b
Daniel Viotor <danielvafranca@gmail.com>
2025-01-12 21:20:46 +0000

    Inicio do progeto

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GardenPro.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PassWordHash { get; set; }
+    public string Slug { get; set; }
+
+    public Ilis<Role> Roles { get; set; }
+
+    public IEnumerable<Claim> GetClaims()
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, Id.ToString()),
+            new Claim(ClaimTypes.Name, Name),
+            new Claim(ClaimTypes.Email, Email)
+        };
+    
+    }
+
+}
